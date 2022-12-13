32737fe97ac8a29f36395962b3e647c901f7f4bf
buharialiyu <119262034+buharialiyu@users.noreply.github.com>
2022-12-13 21:13:05 +0100

    Updated sql database

--- Secure HomeD.sql
+++ Secure HomeD.sql
@@ -1,15 +1,15 @@
 create database if not exists Secure_home;
 use secure_home;
 create table if not exists User(
-userID Integer primary key,
-firstName varchar(255),
+userID Integer AUTO_INCREMENT primary key,
+firstName varchar(255)not null,
 lastName varchar(255) not null,
 age integer check (age>=18),
 phoneNumber varchar(10),
 userName varchar(100) not null,
 emailAddress varchar(50),
 password varchar(50) not null,
-userType varchar(10) not null,
+userType varchar(10),
 check( emailAddress LIKE '%___@___%.__%'),
 check( userType LIKE ('admin' or 'user')),
 streetName varchar(100),
@@ -20,7 +20,7 @@ code VARCHAR(255)
 );
 
 create table if not exists PublicAuthority(
-authorityID integer primary key,
+authorityID integer AUTO_INCREMENT primary key,
 name varchar(60) not null,
 emailAddress varchar(60), 
 check( emailAddress LIKE '%___@___%.__%'),
@@ -28,27 +28,27 @@ address varchar(250),
 phoneNumber varchar(10)
 );
 create table if not exists Message(
-mID integer primary key,
+mID integer AUTO_INCREMENT primary key,
 mTime time,
 content text(3000),
 authorityID integer references PublicAuthority(authorityID),
 userID integer references User(userID)
 );
 create table if not exists Address(
-addressID integer primary key,
+addressID integer AUTO_INCREMENT primary key,
 streetName varchar(100),
 city varchar(100),
 state varchar(100),
 postalCode integer(6)
 );
 create table if not exists Alert(
-alertID integer primary key,
+alertID integer AUTO_INCREMENT primary key,
 aDate date,
 aTime time,
 userID integer references User(userID)
 );
 create table if not exists DeviceStatus(
-devicestatusID integer primary key,
+devicestatusID integer AUTO_INCREMENT primary key,
 dStatus bool not null default false,
 timeActivated time,
 timeDeactivated time,
@@ -56,20 +56,20 @@ deviceID integer references Device(deviceID),
 userID integer references User(userID)
 );
 create table Device(
-deviceID integer primary key,
+deviceID integer AUTO_INCREMENT primary key,
 deviceName varchar(100),
 deviceType varchar(100),
 sID integer references Sensor(sID)
 );
 create table if not exists Sensor(
-sID integer primary key,
+sID integer AUTO_INCREMENT primary key,
 sName varchar(100),
 sType varchar(100),
 sStatus bool,
 sDate date
 );
 create table if not exists SensorData(
-DataID integer primary key,
+DataID integer AUTO_INCREMENT primary key,
 sdTime time,
 Reading decimal(2,2),
 sID integer references Sensor(sID)
